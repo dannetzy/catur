9a6d3a946f0f8b831a88c7a3afd3ff50a2955496
dannetzy <mdannefebrian@gmail.com>
2026-05-19 10:33:09 +0700

    turned game logic functions into a single object, added turn system

--- script.js
+++ script.js
@@ -1,14 +1,88 @@
-const pieces = {
-  king: '♚',
-  queen: '♛',
-  rook: '♜',
-  bishop: '♝',
-  knight: '♞',
-  pawn: '♟'
-};
-const board = document.querySelector('.board');
+const game = {
+  pieces: {
+    king: '♚',
+    queen: '♛',
+    rook: '♜',
+    bishop: '♝',
+    knight: '♞',
+    pawn: '♟'
+  },
+  turn: '',
+  /**
+   * Get piece info from a square
+   * @param {HTMLElement} square The square to get
+   * @returns Object containing the piece's info
+   */
+  getPiece(square) {
+    if (!(square instanceof HTMLElement)) {
+      return {
+        piece: '',
+        color: ''
+      };
+    }
+    return {
+      piece: square.classList[0],
+      color: square.classList[1]
+    };
+  },
+  /**
+   * Updates a square
+   * @param {HTMLElement} square The square's element
+   * @param {String} piece Piece class of the square's element, blank to empty out the square
+   * @param {String} color Color class of the square's element, blank to empty out the square
+   */
+  updateSquare(square, piece, color) {
+    // wipe classes
+    for (const cls of [...square.classList]) { 
+      square.classList.remove(cls);
+    }
+    if (!piece || !color) {
+      square.textContent = '';
+      return;
+    }
+    square.textContent = this.pieces[piece];
+    square.classList.add(piece, color);
+  },
+  /**
+   * Set a square as selected
+   * @param {HTMLElement} square Square to select
+   */
+  setSelected(square) {
+    square.classList.add('selected');
+  },
+  /**
+   * Clears selected piece
+   * @param {Boolean} unselect Truthy to only unselect the selected piece without removing
+   */
+  clearSelected(unselect) {
+    const selected = document.querySelector('.selected');
+    if (!selected) {
+      return;
+    }
+    selected.classList.remove('selected');
+    if (!unselect) {
+      this.updateSquare(selected);
+    }
+  },
+  updateTurn(color, initColor) {
+    if (color === 'init' && initColor) {
+      this.turn = initColor;
+      turnTd.classList.add(initColor);
+      turnText.textContent = initColor;
+      return;
+    }
+    this.turn = color === 'white' ? 'black' : 'white';
+    turnTd.classList.remove(color);
+    turnTd.classList.add(this.turn);
+    turnText.textContent = this.turn;
+  },
+  isTurn(color) {
+    return color === this.turn;
+  }
+}
 
 //initialize board
+const board = document.querySelector('.board');
 const pieceRow = document.querySelectorAll('.pieces');
 const pawnRow = document.querySelectorAll('.pawns');
 
@@ -16,72 +90,21 @@ pieceRow.forEach((tr, i) => {
   const rowOfPieces = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook'];
   const colSquares = [...tr.children].splice(1); // remove th
   colSquares.forEach((td, j) => {
-    updateSquare(td, rowOfPieces[j], i ? 'white' : 'black') // 0: first = black, 1: last = white
+    game.updateSquare(td, rowOfPieces[j], i ? 'white' : 'black') // 0: first = black, 1: last = white
   });
 });
 pawnRow.forEach((tr, i) => {
   const colSquares = [...tr.children].splice(1);
   colSquares.forEach(td => {
-    updateSquare(td, 'pawn', i ? 'white' : 'black');
+    game.updateSquare(td, 'pawn', i ? 'white' : 'black');
   });
 });
 
-/**
- * Get piece info from a square
- * @param {HTMLElement} square The square to get
- * @returns Object containing the piece's info
- */
-function getPiece(square) {
-  if (!(square instanceof HTMLElement)) {
-    return {
-      piece: '',
-      color: ''
-    }
-  }
-  return {
-    piece: square.classList[0],
-    color: square.classList[1]
-  };
-}
-/**
- * Updates a square
- * @param {HTMLElement} square The square's element
- * @param {String} piece Piece class of the square's element, blank to empty out the square
- * @param {String} color Color class of the square's element, blank to empty out the square
- */
-function updateSquare(square, piece, color) {
-  // wipe classes
-  for (const cls of [...square.classList]) { 
-    square.classList.remove(cls);
-  }
-  if (!piece || !color) {
-    square.textContent = '';
-    return;
-  }
-  square.textContent = pieces[piece];
-  square.classList.add(piece, color);
-}
-/**
- * Set a square as selected
- * @param {HTMLElement} square Square to select
- */
-function setSelected(square) {
-  square.classList.add('selected');
-}
-/**
- * Clears selected piece
- * @param {Boolean} unselect Truthy to only unselect the selected piece without removing
- */
-function clearSelected(unselect) {
-  const selected = document.querySelector('.selected');
-  if (!selected) {
-    return;
-  }
-  selected.classList.remove('selected');
-  if (!unselect) {
-    updateSquare(selected);
-  }
-}
+const turnTd = document.querySelector('.turn-td');
+const turnText = document.querySelector('.turn-text');
+const firstPlayer = 'black';
+
+game.updateTurn('init', firstPlayer);
 
 board.addEventListener('click', (ev) => {
   if (!ev.target.closest('td')) {
@@ -89,17 +112,24 @@ board.addEventListener('click', (ev) => {
   }
 
   const square = ev.target;
-  const selected = getPiece(document.querySelector('.selected'));
-  const clicked = getPiece(square);
+  const selected = game.getPiece(document.querySelector('.selected'));
+  const clicked = game.getPiece(square);
 
   if (selected.piece) {
-    if (clicked.color === selected.color) { //eat same color prevention
-      clearSelected(true);
+    if (clicked.color === selected.color) { //same color taking prevention
+      game.clearSelected(true);
       return;
     }
-    updateSquare(square, selected.piece, selected.color);
-    clearSelected();
+    if (game.isTurn(selected.color)) {
+      game.updateSquare(square, selected.piece, selected.color);
+      game.clearSelected();
+      game.updateTurn(selected.color)
+    } else {
+      alert('not your turn');
+      game.clearSelected(true);
+      return;
+    }
   } else if (clicked.piece) {
-    setSelected(square);
+    game.setSelected(square);
   }
 });
